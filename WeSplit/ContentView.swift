a4734fb41651af3ca582c61be221c22783f8d8a3
Mammetnazar <nazar.rudn@gmail.com>
2023-10-30 09:30:09 +0300

    Deleted the previous code (can be found at a seperate branch, called "example")
    Fisrt steps to our new App, called WeSplit
    
    For now it shows the amount we entered right below the textfield

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,21 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let students = ["Anna", "Nazar", "Maysa"]
-    @State private var selectedStudent = "Nazar"
+    @State private var checkAmount = 0.0
+    @State private var tipPercentage = 20
+    @State private var numberOfPeople = 2
+    
+    let percentageOptions = [0,5,10,15,20]
     
     var body: some View {
-        NavigationStack{
+
             Form{
-                Picker("Who is your favorite student?", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        
                 }
                 
-
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-        }
     }
     }
 
